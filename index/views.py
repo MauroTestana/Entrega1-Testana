2d2e64453a8f55cfe172d8d43e3d0e812fe61258
Mauro Testana <mauro.tes99@hotmail.com>
2022-03-08 01:32:53 -0300

    actualizacion 3

--- index/views.py
+++ index/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('<h1>Esta es una pag de django</h1>')
+
+def plantilla(request):
+    
+    template = loader.get_template('plantilla.html')
+    
+    datos = {
+        'lista': ['primero', 'segundo', 'tercero'],
+        'nombre': 'Juan'
+    }
+    
+    plantilla_generada = template.render(datos)
+    
+    return HttpResponse(plantilla_generada)
