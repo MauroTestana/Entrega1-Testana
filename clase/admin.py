ea1347051a4f3f63996dd249166ecdc52d3aeadd
Mauro Testana <mauro.tes99@hotmail.com>
2022-03-13 21:41:16 -0300

    act10

--- clase/admin.py
+++ clase/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from clase.models import Curso, Entegrables, Estudiantes, Profesor
+
 # Register your models here.
+
+admin.site.register(Estudiantes)
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Entegrables)
