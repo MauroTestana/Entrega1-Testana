6b9b23909ad1f991442d58f3b158cb89351b0cce
Mauro Testana <mauro.tes99@hotmail.com>
2022-04-01 22:31:30 -0300

    act32

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from accounts.forms import NuestraCreacionUser
 from django.contrib.auth import authenticate, login as django_login
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import NuestraCreacionUser
+from .forms import NuestraCreacionUser, EditFullUser
+from django.contrib.auth.decorators import login_required
+from .models import UserExtention
 
 # Create your views here.
 
@@ -48,4 +50,34 @@ def registrar(request):
              return render(request, 'accounts/registrar.html', {'form':form, 'msj': ''})    
     
     form = NuestraCreacionUser()
+    return render(request,  'accounts/registrar.html', {'form': form, 'msj':''})
+
+@login_required
+def editar_usuario(request):
+    
+    UserExtension.objects.filter(user=request.user)
+    
+    
+    if request.method == 'POST':
+        form = EditFullUser(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            form.save()
+            return render(request, 'index/index.html', {'msj': f'Se creo el usuario {username}'})
+        else:
+             return render(request, 'accounts/registrar.html', {'form':form, 'msj': ''})    
+    
+    form = EditFullUser(
+        initial={
+            'email': request.user.email, 
+            'password1'  
+            'password2'  
+            'first_name' : request.user.first_name, 
+            'last_name' : request.user.last_name,  
+            'avatar'  
+            'link' = 
+            'more_description' = 
+        }
+    )
     return render(request,  'accounts/registrar.html', {'form': form, 'msj':''})
