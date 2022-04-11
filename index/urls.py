157105111d7c78125f739728c51531d150d397d3
Mauro Testana <mauro.tes99@hotmail.com>
2022-04-10 22:05:36 -0300

    ultimos detalles

--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index 
+from .views import index, about
 #from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', index, name='index')
-    #path('plantilla/', plantilla, name='plantilla'),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
     #path('login/', login  , name='login'),
     #path('registrar/', registrar, name='registrar'),
     #path('logout/', LogoutView.as_view(template_name='index/logout.html'), name='logout')
